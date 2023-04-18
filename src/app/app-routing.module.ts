09b9091fe6f1a5885bbf2004119ee3063aeba4e1
ajaysingh8462 <ajaysingh84625@gmail.com>
2023-04-18 06:42:08 +0530

    [ajay- dashboard and doctors page]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { DashboradComponent } from './components/dashborad/dashborad.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
 
-const routes: Routes = [
+const routes: Routes = [ 
+  {path:'', redirectTo:'signin',pathMatch: 'full'},
   {path: 'signup',component: SignupComponent},
   {path: 'signin',component: SigninComponent},
+  {path:'dashborad',component:DashboradComponent,children:[{path:'doctors',component:DoctorsComponent}]}
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { SigninComponent } from './components/signin/signin.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboradComponent } from './components/dashborad/dashborad.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    SigninComponent
+    SigninComponent,
+    DoctorsComponent,
+    DashboradComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
